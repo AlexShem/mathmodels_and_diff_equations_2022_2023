1eb011e8ea613cbbd10b867b1c1e577c0c5d5fe8
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-09-28 20:58:24 +0200

    [New] improved main

--- Seminar 01 - week 1/main.m
+++ Seminar 01 - week 1/main.m
@@ -1,28 +1,27 @@
 %% Initial conditions
-
-% Speed
+% Speed function u(t, x)
 a = 2; b = 1;
 
-% u = @(t, x) a*x + b;
-% dudt = @(t, x) zeros(size(x));
-% dudx = @(t, x) a*ones(size(x));
+u = @(t, x) a*x + b;
+dudt = @(t, x) zeros(size(x));
+dudx = @(t, x) a*ones(size(x));
 
-u = @(t, x) -a*x/200 + .5*t.*x;
-dudt = @(t, x) 2*pi*sin(2*pi*t);
-dudx = @(t, x) a*pi*cos(pi*x);
+% u = @(t, x) -a*x/200 + .5*t.*x;
+% dudt = @(t, x) .5*x;
+% dudx = @(t, x) -a/200 + .5*t;
 
-% Integration time
+%% Integration time
 T = 1;
 t = linspace(0, T, 11);
 
 % Segment of interest
-x = linspace(-1, 1, 11);
+x = linspace(-1, 1, 51);
 
 % Initial density distribution
 rho_fun = @(x) .5*normpdf(x, 0, .5) + ...
     .25*normpdf(x, -.5, .25) + ...
     .25*normpdf(x, .5, .25);
-rho = rho_fun(x);
+rho_0 = rho_fun(x);
 
 
 figure(1);
@@ -31,7 +30,6 @@ xlabel('$x$', Interpreter = 'latex', FontSize = 16);
 legend('$\rho(0, x)$', Interpreter = 'latex', FontSize = 16);
 
 %% Characteristics
-
 x0 = zeros(size(x));
 
 figure(2)
@@ -49,11 +47,40 @@ hold off;
 xlabel('$x$', Interpreter = 'latex', FontSize = 16);
 ylabel('$t$', Interpreter = 'latex', FontSize = 16);
 
-%% Interpolation of rho at x0
-rho_0 = spline(x, rho, x0);
+%% Interpolation of the density function at zeroth time
+% Cubic interpolation
+rho_0_app = spline(x, rho_0, x0);
 
 figure(3)
 fplot(rho_fun, [min([x, x0]), max([x, x0])]);
 hold on;
-plot(x0, rho_0, '*r');
+plot(x0, rho_0_app, '*r');
 hold off;
+xlabel('$x$', Interpreter = 'latex', FontSize = 16);
+legend('$\rho(0, x)$', '$\rho(0, x)$ interpolated', Interpreter = 'latex', FontSize = 16, Location = 'best');
+
+%% Conservation of mass equation
+% figure(2)
+rho = zeros(1, length(x));
+
+for k = 1 : length(x)
+    [t_path, x_path] = ode45(u, [T 0], x(k));
+    app_rho_0 = spline(x, rho_0, x_path(end));
+
+    dudx_path = dudx(t_path, x_path);
+    dpath = [diff(x_path), diff(t_path)];
+    h = sqrt(sum(dpath.^2, 2));
+
+    integ = .5*(dudx_path(2:end) + dudx_path(1:end-1)).' * h;
+    rho(k) = app_rho_0 * exp(-integ);
+end
+
+%% Density change visualisation
+figure(3)
+plot(x, rho_0, '-s');
+hold on;
+plot(x, rho, '-d');
+hold off;
+legend('$\rho(0, x)$', '$\rho(1, x)$', ...
+    Interpreter = 'latex', FontSize = 16, Location = 'best');
+xlabel('$x$', Interpreter = 'latex', FontSize = 16)
