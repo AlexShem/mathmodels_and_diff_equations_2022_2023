7fee55cb515ff7a850e214167685ae6797fbac9c
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2023-04-12 20:59:03 +0200

    [New] Leibenson

---
+++ Seminar 11 - week 14/integration.m
@@ -0,0 +1,26 @@
+function [U, Nt, T, x, h, t] = integration(L, T, D, Nx, tau, nu, u_0, scheme)
+%% Secondary paramters
+x = linspace(0, L, Nx + 1);
+h = x(2) - x(1);
+
+Nt = ceil(T/tau) + 1;
+T = tau*(Nt - 1);
+t = 0 : tau : T;
+
+%% Integration
+U = zeros(Nt, Nx);
+U(1, :) = u_0(x(1 : end-1));
+
+for k = 2 : Nt
+    u = U(k-1, :);
+    u_ex = adams_bashforth(u, D, tau, h);
+    if strcmp(scheme, 'compact')
+        comp_eps = compact_correction(u, u_ex, D, h, tau);
+    else
+        comp_eps = zeros(length(u), 1);
+    end
+    u_k = u_ex + comp_eps.';
+    U(k, :) = u_k;
+end
+U = [U, U(:, 1)];
+end
